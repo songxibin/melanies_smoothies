60d46bc3641051cc2a789d3f8d9a7f5912754558
songxibin <songxibin@gmail.com>
2025-01-06 13:49:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients ', my_dataframe, max_selections=5)
 if ingredients_list:
